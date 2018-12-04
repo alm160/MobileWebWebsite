8930bedefafd42d8fd1cf4da0b2453764bdcf585
alm160 <45599332+alm160@users.noreply.github.com>
2018-12-04 10:24:04 -0500

    Add files via upload

---
+++ board.php
@@ -0,0 +1,52 @@
+<!DOCTYPE>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<title>Homepage</title>
+
+	<link rel="stylesheet" href="css/mainMobile.css" type="text/css">
+<?php
+	include 'includes/mobileNav.php';
+?>
+</head>
+<body>
+	<div id="mainHeader">
+		<h1>Board Game Examples</h1>
+	</div>
+
+		<div class="header">
+			<h2>Pandemic</h2>
+		</div>
+			<div class="body">
+				<p>
+					As skilled members of a disease-fighting team, you and the other players work together to keep the world safe from outbreaks and epidemics. Only through teamwork will you have a chance to find a cure. Pandemic is a cooperative board game in which players work as a team to treat infections around the world while gathering resources for cures. First published in 2007, the game's unique combination of cooperative gameplay, engrossing premise, and compelling design have proved a hit with everyone from hardcore gamers to casual players. The Pandemic game line now includes multiple expansions and stand-alone titles.
+				<div>
+					<p>
+						<a href="#top">Back to top of page</a>
+					</p>	
+				</div>
+			</div>
+		<div class="header">
+			<h2>Scythe</h2>
+		</div>
+			<div class="body">
+				<p>
+					Scythe (1-5 players, 115 minutes) is a board game set in an alternate-history 1920s period. It is a time of farming and war, broken hearts and rusted gears, innovation and valor. In Scythe, each player represents a fallen leader attempting to restore their honor and lead their faction to power in Eastern Europa. Players conquer territory, enlist new recruits, reap resources, gain villagers, build structures, and activate monstrous mechs.
+				</p>
+				<div>
+					<p>
+						<a href="#top">Back to top of page</a>
+					</p>	
+				</div>
+			</div>
+</body>
+<footer>
+	<?php
+		include 'includes/mobileFooter.php';
+	?>
+</footer>
+</html>
